85d4223ab00b220416f25b1a8b945c8a622876ba
Shivam-Pandey-07 <90436943+Shivam-Pandey-07@users.noreply.github.com>
2021-09-21 13:48:14 +0530

    Add files via upload

---
+++ array3x3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+ 	int i,j;
+	int rows,columns;
+	int a[10][10];
+	int Sum = 0;
+ 	printf("\n Please Enter Number of rows and columns:");
+ 	scanf("%d %d", &i, &j);
+ 	printf("\n Please Enter the Matrix Elements \n");
+ 	for(rows = 0; rows < i; rows++)
+  	{
+   		for(columns = 0;columns < j;columns++)
+    	{
+      		scanf("%d", &a[rows][columns]);
+    	}
+  	}  
+ 	for(rows = 0; rows < i; rows++)
+  	{
+   		Sum = Sum + a[rows][rows];
+  	}
+ 	printf("\n The Sum of Diagonal Elements of a Matrix =  %d", Sum );
+ 	return 0;
+}
